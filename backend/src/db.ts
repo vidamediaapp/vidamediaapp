1f2e758111a408669ec7cb326d3cbc77ab5e04be
Roxellana <diegohuanchicaycruz@gmail.com>
2026-07-07 02:51:42 -0400

    correcion conexion base de datos

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -15,10 +15,10 @@ dotenv.config();
 export const AppDataSource = new DataSource({
     type: 'postgres',
     host: process.env.DB_HOST || 'localhost',
-    port: parseInt(process.env.DB_PORT || '5432'),
+    port: parseInt(process.env.DB_PORT || '6543'),
     username: process.env.DB_USER || 'postgres',
     password: process.env.DB_PASSWORD || 'VidaMedia2026',
-    database: process.env.DB_NAME || 'VidaMediadb',
+    database: process.env.DB_NAME || 'vidamediadb',
     entities: [
         Usuario,
         Acreedor,
